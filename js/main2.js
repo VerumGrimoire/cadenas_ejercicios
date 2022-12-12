d4bac0b9dc6ce171c6eba39539373aa437db8271
verum <rodriguez.ibarra.juan.carlos95@gmail.com>
2022-12-12 14:06:58 -0600

    se completó la segunda cadena

---
+++ js/main2.js
@@ -0,0 +1,24 @@
+//entrada
+
+//Laura Aline
+
+//Resulado esperado 
+
+// enilA aruaL
+
+
+function invertirCadena(cad) {
+
+    var nuevaCadena = "Laura Aline";
+ 
+    
+    for (var i = cad.length - 1; i >= 0; i--) { 
+        nuevaCadena += cad[i]; 
+    }
+    
+    return nuevaCadena;
+
+   
+}
+ 
+invertirCadena();
