682764e17ba2d07f80e8df71b3f94653373c546d
verum <rodriguez.ibarra.juan.carlos95@gmail.com>
2022-12-12 13:48:08 -0600

    Se soluciona el primer ejercicio con strings

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
 //Ejercicios String/Cadenas 12/12/22
 
-Let 
+//entontrar el caracter que más se repite
+{let nombre = "Abraham"
+
+let char = nombre.charAt();
+nombre = nombre.toLocaleUpperCase();
+let duplicados1 = [];
+
+for (let contador = 0; contador < char; contador++){
+    if (char[contador] === char[contador+1]) {
+        duplicados1.push(char[contador]);
+    }
+    if (char[contador+1] === char[contador+2]) {
+        duplicados1.push(char[contador]);
+    }
+    if (char[contador+3] === char[contador+4]) {
+        duplicados1.push(char[contador]);
+    }
+    if (char[contador+4] === char[contador+5]) {
+        duplicados1.push(char[contador]);
+    }
+    if (char[contador+5] === char[contador+6]) {
+        duplicados1.push(char[contador]);
+    }
+}
+
+
+
+console.log(duplicados1);
+
+
+}
+
+let nombre = "Abraham"
+nombre = nombre.toUpperCase();
+let letra="";
+let contFinal=0;
+for (let index =0; index < nombre.length; index++) {
+    console.log(nombre.charAt(index));
+    let contTemp=1;
+    for (let cont = index+1; cont < nombre.length; cont++) {
+        if(nombre.charAt(index) == nombre.charAt(cont))
+            contTemp++;
+    }
+    if (contTemp>contFinal) {
+        contFinal=contTemp;
+        letra = nombre.charAt(index);
+    }
+}
+
+console.log(letra, contFinal);
