db1db27c9f36a85c4a7c529d8707191de661fbfe
verum <rodriguez.ibarra.juan.carlos95@gmail.com>
2022-12-13 10:55:54 -0600

    Se agregan validaciones al campo RFC

--- js/main3.js
+++ js/main3.js
@@ -5,7 +5,8 @@ btnEnviar.addEventListener("click", function(event){
     event.preventDefault();
     let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     
-    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1"); //email
+    let exampleFormControlInput2 = document.getElementById("exampleFormControlInput2"); //rfc
     let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
     let alertError = document.getElementById("alertError");
     exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
@@ -22,6 +23,12 @@ btnEnviar.addEventListener("click", function(event){
     if (exampleFormControlInput1.value.match(emailRegex) == null) {
         alertError.style.display="block";
         alertError.innerHTML += "<br/>El correo electrónico no es válido.";
+        
+    }
+    let RFCRegex =;
+    if (exampleFormControlInput2.value.match(RFCRegex) == null) {
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El RFC no es válido.";
         
     }
 
